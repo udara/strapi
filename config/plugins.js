de3ff2c59404205ab6713e9cb7cad80b5d5d49c2
Udara Ranasinghe <indi79@hotmail.com>
2023-07-06 15:50:09 +0930

    sada

--- config/plugins.js
+++ config/plugins.js
@@ -6,7 +6,7 @@ module.exports = ({ env }) => ({
       providerOptions: {
         key: 'DO00XTGUBPZK9XEUD3AY',
         secret: 'HFoRH8gZwwcuCth+cD4uJd4lhLZHdiBRCOK7sFPX178',
-        endpoint: 'https://udara-strapi-test.syd1.digitaloceanspaces.com',
+        endpoint: 'syd1.digitaloceanspaces.com',
         space: 'udara-strapi-test',
         directory: "media", // optional
       },
